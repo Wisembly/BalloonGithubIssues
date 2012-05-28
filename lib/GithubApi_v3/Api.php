8f7326cdff3780ca08366de1c428e3a4b86999d9
Charles-Emmanuel <Charles-Emmanuel.Dias@etu.upmc.fr>
2012-05-28 13:06:45 +0200

    -method to get list of repository labels added

--- lib/GithubApi_v3/Api.php
+++ lib/GithubApi_v3/Api.php
@@ -18,6 +18,10 @@ class Api {
     {
         return $this->get('/repos/'.$user.'/'.$repo.'/milestones', $this->params($params));
     }
+	public function getLabels($user, $repo, $params = array())
+    {
+	return $this->get('/repos/'.$user.'/'.$repo.'/labels', $this->params($params));
+    }
 
     public function loadUserData()
     {
